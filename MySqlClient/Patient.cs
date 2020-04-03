b1bc95e5c0dbbdf3e6d514c55de2fa37a7ee862b
Dzirit <41745559+Dzirit@users.noreply.github.com>
2020-04-03 16:57:23 +0300

    Добавьте файлы проекта.

---
+++ MySqlClient/Patient.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MySqlClient
+{
+    class Patient
+    {
+        public Guid PublicIdGuid { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string DateOfBirth { get; set; }
+        public string DateOfRegistration { get; set; }
+        public string Sex { get; set; }
+        public string Institute { get; set; }
+        public string Department { get; set; }
+        public string Doctor { get; set; }
+        public string Id { get; set; }
+        public string Node { get; set; }
+    }
+}
---
+++ MySqlClient/Program.cs
@@ -0,0 +1,40 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Threading.Tasks;
+
+namespace MySqlClient
+{
+    class Program
+    {
+        const string SqlGetPcnt = "SELECT * FROM pcnt";
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Openning Connection ...");
+            await ConnectDB();
+            Console.WriteLine("Done.");
+
+        }
+        private static async Task ConnectDB()
+        {
+            string connStr = "server=localhost;user=PCNTDB;database=start_db;port=3309;password=123456";
+
+            using (var conn = new MySqlConnection(connStr))
+            {
+                await conn.OpenAsync();
+                Console.WriteLine($"Connection is {conn.State}");
+                await ReadTable(conn, SqlGetPcnt);
+            }
+
+        }
+        private static async Task ReadTable(MySqlConnection conn,string sqlRequest)
+        {
+            // Retrieve all rows
+            using (var cmd = new MySqlCommand(sqlRequest, conn))
+            using (var reader = await cmd.ExecuteReaderAsync())
+                while (await reader.ReadAsync())
+                    Console.WriteLine($"{reader[1]}");
+            await conn.CloseAsync();
+
+        }
+    }
+}
